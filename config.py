e4f7d3f2e7efc30f320aad43f213f26065d1f1b4
Sumi Loo <lannsnolo@gmail.com>
2025-02-28 19:57:34 +0545

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", "7765142028:AAGPRPYB-KBw239ZOqx_U2fREn7ZPW0FYdc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://sumiloo:<db_password>@react.fvqrg.mongodb.net/?retryWrites=true&w=majority&appName=react")
 OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
+SUPPORT_GRP = "https://t.me/suMelody_vibes"
+UPDATE_CHNL = "https://t.me/+VVeCnUdiTGg0MjZl"
 OWNER_USERNAME = "@dear_sumi"
     
